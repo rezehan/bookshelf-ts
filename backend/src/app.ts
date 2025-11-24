5d0a7dbaa22519a73349790c9522d3caefd9ae67
Raihan Abdillah <adraihan94@gmail.com>
2025-11-24 13:53:19 +0800

    fullstack

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,13 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+const books = [];
+
+// added book
+app.post("/books", (req, res) => {
+  
+})
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello from Express + TS" });
 });
